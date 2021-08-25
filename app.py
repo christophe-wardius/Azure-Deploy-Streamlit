ceb60d7734499d87fd893017e8371e93b37347c1
Christophe Wardius <83533458+christophe-wardius@users.noreply.github.com>
2021-08-25 10:10:44 +0200

    Add files via upload

---
+++ app.py
@@ -0,0 +1,16 @@
+import streamlit as st
+
+def main():
+    st.sidebar.markdown("""
+        # Navigation
+        this is where navigation stuff should go
+    """)
+
+    st.markdown("""
+    # My Title
+    
+    This is where the content should go
+    """)
+
+if __name__ == "__main__":
+    main()
